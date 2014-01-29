8a14393c6a644a0ab4945e05181f55eea88888b7
dobestan <dobestan@gmail.com>
2014-01-29 13:59:55 +0000

    rails generate controller User new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
