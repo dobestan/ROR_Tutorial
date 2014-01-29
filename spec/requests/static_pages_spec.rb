1e3d46941d9b5fd1180239f430b0ddc4661c3169
dobestan <dobestan@gmail.com>
2014-01-29 07:32:31 +0000

    update help.html.erb : test passed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,11 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
 end
