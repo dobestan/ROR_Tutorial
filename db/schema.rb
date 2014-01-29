41a3fed9bdf37e31ac06e9cde56d561d865e9fce
dobestan <dobestan@gmail.com>
2014-01-29 15:25:34 +0000

    update user model : email unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129141908) do
+ActiveRecord::Schema.define(version: 20140129152017) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20140129141908) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
