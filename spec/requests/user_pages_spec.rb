4f7074cbd8dbbb82fb39ac31911a9038a5b998b5
dobestan <dobestan@gmail.com>
2014-01-30 02:08:47 +0000

    update test : user profile pate

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,11 @@ describe "User Pages" do
 
     it { should have_content('Sign up') }
   end
+
+  describe "profile page" do 
+    before { visit user_path{user} }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
