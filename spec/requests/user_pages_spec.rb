2aa58bca7db986f05c45c57c4f2613b44cad2a97
dobestan <dobestan@gmail.com>
2014-01-30 02:55:45 +0000

    update test : user signup validate

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,4 +16,29 @@ describe "User Pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do 
+      before do
+        fill_in "Name",             with: "dobestan"
+        fill_in "Email",             with: "dobestan@gmail.com"
+        fill_in "Password",             with: "foobar"
+        fill_in "Confirmation",             with: "foobar"
+      end
+
+      it "should create a user" do 
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
