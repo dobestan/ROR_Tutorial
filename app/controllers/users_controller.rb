060eb937d4b7bf4748e75c671252e697c366ee3f
dobestan <dobestan@gmail.com>
2014-01-30 03:17:15 +0000

    add CREATE action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
