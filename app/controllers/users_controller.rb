cda4b8aaa30f2a7705cd192b7b58ff361dfcf25b
dobestan <dobestan@gmail.com>
2014-01-31 13:30:35 +0000

    add EDIT/UPDATE action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a succesfful update
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,4 +50,22 @@ describe "User Pages" do
       end
     end
   end
+
+  #EDIT
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link("change", href: "http://gravatar.com/emails" ) }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content("error")}
+    end
+  end
 end
