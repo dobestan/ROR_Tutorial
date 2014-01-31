de4121340692ba3ec66ba8e0aec27ef8ce0868e2
dobestan <dobestan@gmail.com>
2014-01-31 08:01:10 +0000

    add flash msg and test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
