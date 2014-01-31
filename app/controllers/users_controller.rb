f77ba8a0d19ef718cfc5e39652990b9bc00a07f1
dobestan <dobestan@gmail.com>
2014-01-31 13:47:34 +0000

    update EDIT action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # Handle a succesfful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,7 +54,10 @@ describe "User Pages" do
   #EDIT
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_content("Update your profile") }
@@ -66,6 +69,26 @@ describe "User Pages" do
       before { click_button "Save changes" }
 
       it { should have_content("error")}
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link("Sign out", href: signout_path) }
+
+      specify { expect(user.reload.name).to eq new_name}
+      specify { expect(user.reload.email).to eq new_email}
     end
   end
 end
