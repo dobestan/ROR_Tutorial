2380c78edeced1386cdb2249ffcc5ec73a310dce
dobestan <dobestan@gmail.com>
2014-01-31 07:35:27 +0000

    update signin test : to have valid form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,5 +7,26 @@ describe "Authentication" do
 
     it { should have_content("Sign in") }
     it { should have_title("Sign in") }
+
+    describe "with invalid information" do
+      before {click_button "Sign in"}
+
+      it {should have_title("Sign in")}
+      it {should have_selector("div.alert.alert-error")}
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user)}
+      before do
+        fill_in "Email",      with: user.email.upcase
+        fill_in "Password",      with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',      href: user_path(user)) }
+      it { should have_link('Sign out',      href: signout_path) }
+      it { should_not have_link('Sign in',      href: signin_path) }
+    end
   end
 end
