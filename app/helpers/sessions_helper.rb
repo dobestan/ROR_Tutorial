5d356aac6dda05497a9b1ee1b74d32188c998b68
dobestan <dobestan@gmail.com>
2014-02-01 04:39:30 +0000

    add NEW action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,6 +39,13 @@ module SessionsHelper
     session.delete(:return_to)
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def store_location
     session[:return_to] = request.url if request.get?
   end
